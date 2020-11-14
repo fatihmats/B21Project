b38b40194f12f4c4a533bab869405b1b3f1c5a81
fatihmats <fatihmats@gmail.com>
2020-11-14 11:45:47 -0500

    Find Max Number Of array

---
+++ src/FindMaxNumbers.java
@@ -0,0 +1,16 @@
+public class FindMaxNumbers {
+    public static void main(String[] args) {
+        int [][] arr = {{1,3,5}, {7,-5,18},{-2,3,7}};
+        int max = arr[0][0];
+        for (int[] each:arr) {
+            for (int each1:each) {
+                if (each1>max){
+                    max=each1;
+                }
+
+            }
+
+        }
+        System.out.println("Max number is : "+max);
+    }
+}
